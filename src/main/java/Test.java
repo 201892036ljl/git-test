9bb343b052328f9b169bc903d9346d74eae53fd5
201892036ljl <73569975+201892036ljl@users.noreply.github.com>
2022-09-28 15:21:02 +0800

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,6 +6,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("1112211");
         System.out.println("11111");
-        System.out.println("22222");
+        System.out.println(1 + 2);
     }
 }
